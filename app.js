a0a4fe4fd8191c97514df2fd36cce5e1998e0d61
salman261002 <116475964+MuhammadSalmanAlfarisi@users.noreply.github.com>
2023-06-13 19:45:34 +0700

    feats: Search by Name, ID, Boolean feature

--- app.js
+++ app.js
@@ -1,135 +1,187 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // MySQL
 const pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'password',
-    database        : 'nodejs_beers'
-})
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodejs_beers",
+});
 
 // Get all beers
-app.get('', (req, res) => {
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+    connection.query("SELECT * from beers", (err, rows) => {
+      connection.release(); // return the connection to pool
 
-        connection.query('SELECT * from beers', (err, rows) => {
-            connection.release() // return the connection to pool
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+        res.status(500).send("Error retrieving beers");
+      }
+    });
+  });
+});
 
-            if(!err) {
-                res.send(rows)
-            } else {
-                console.log(err)
-            }
+// Search Beer
+app.get("/search/:query", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-        })
-    })
-})
+    const search = req.params.query;
+    connection.query(
+      "SELECT * FROM beers WHERE name LIKE ? OR tagline LIKE ?",
+      [`%${search}%`, `%${search}%`],
+      (err, rows) => {
+        connection.release(); // return the connection to the pool
 
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+          res.status(500).send("Error retrieving beers");
+        }
+      }
+    );
+  });
+});
 
 // Get a beer by ID
-app.get('/:id', (req, res) => {
+app.get("/beers/:id", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+    connection.query(
+      "SELECT * from beers WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
 
-        connection.query('SELECT * from beers WHERE id = ?', [req.params.id], (err, rows) => {
-            connection.release() // return the connection to pool
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+          res.status(500).send("Error retrieving beers");
+        }
+      }
+    );
+  });
+});
 
-            if(!err) {
-                res.send(rows)
-            } else {
-                console.log(err)
-            }
+// Get beers with isAvailable parameter
+app.get("/beers/available/:avail", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-        })
-    })
-})
+    const avail = req.params.avail;
+    connection.query(
+      "SELECT * FROM beers WHERE isAvailable = ?",
+      [avail],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
+
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+          res.status(500).send("Error retrieving beers");
+        }
+      }
+    );
+  });
+});
 
 // Delete a records / beer
-app.delete('/:id', (req, res) => {
+app.delete("/delete/:id", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+    connection.query(
+      "DELETE from beers WHERE id = ?",
+      [req.params.id],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
 
-        connection.query('DELETE from beers WHERE id = ?', [req.params.id], (err, rows) => {
-            connection.release() // return the connection to pool
-
-            if(!err) {
-                res.send(`Beer with the Record ID: ${[req.params.id]} has been removed.`)
-            } else {
-                console.log(err)
-            }
-
-        })
-    })
-})
-
+        if (!err) {
+          res.send(
+            `Beer with the Record ID: ${[req.params.id]} has been removed.`
+          );
+        } else {
+          console.log(err);
+          res.status(500).send("Error deleting beers");
+        }
+      }
+    );
+  });
+});
 
 // Add a record / beer
-app.post('', (req, res) => {
+app.post("/create", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+    const params = req.body;
 
-        const params = req.body
+    connection.query("INSERT INTO beers SET ?", params, (err, rows) => {
+      connection.release(); // return the connection to pool
 
-        connection.query('INSERT INTO beers SET ?', params , (err, rows) => {
-            connection.release() // return the connection to pool
+      if (!err) {
+        res.send(`Beer with the name: ${params.name} has been added.`);
+      } else {
+        console.log(err);
+        res.status(500).send("Error creating beers");
+      }
+    });
 
-            if(!err) {
-                res.send(`Beer with the name: ${params.name} has been added.`)
-            } else {
-                console.log(err)
-            }
-
-        })
-
-        console.log(req.body)
-    })
-})
-
+    console.log(req.body);
+  });
+});
 
 // Update a record / beer
-app.put('', (req, res) => {
+app.put("/update", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
 
-    pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+    const { id, name, tagline, description, image } = req.body;
 
-        const { id, name, tagline, description, image } = req.body
+    connection.query(
+      "UPDATE beers SET name = ?, tagline = ?, description = ?, image = ? WHERE id = ?",
+      [name, tagline, description, image, id],
+      (err, rows) => {
+        connection.release(); // return the connection to pool
 
-        connection.query('UPDATE beers SET name = ?, tagline = ?, description = ?, image = ? WHERE id = ?', [name, tagline, description, image, id] , (err, rows) => {
-            connection.release() // return the connection to pool
+        if (!err) {
+          res.send(`Beer with the name: ${name} has been updated.`);
+        } else {
+          console.log(err);
+          res.status(500).send("Error updating beers");
+        }
+      }
+    );
 
-            if(!err) {
-                res.send(`Beer with the name: ${name} has been added.`)
-            } else {
-                console.log(err)
-            }
-
-        })
-
-        console.log(req.body)
-    })
-})
-
-
+    console.log(req.body);
+  });
+});
 
 // Listen on enviroment port or 5000
-app.listen(port, () => console.log(`Listen on port ${port}`))
+app.listen(port, () => console.log(`Listen on port ${port}`));
